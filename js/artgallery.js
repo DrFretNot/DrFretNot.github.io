752d111a99b628b61a15334bb34194687a617f3c
Trent Ford <thetrentford@rocketmail.com>
2021-12-22 22:36:12 -0600

    Update Navbar and Preliminary Art Gallery

---
+++ js/artgallery.js
@@ -0,0 +1,109 @@
+/*** Art Gallery ****/
+//product_greatball.png , product_pokeball.png , product_ultraball.png
+/*
+abstract_brushstroke.png , 
+abstract_professional_hats.png , 
+abstract_professional_hats_bg.png , 
+	comic_gamera.png , 
+	creature_armorsnake.png , 
+	creature_armorsnake_lineart.png , 
+	creature_bee.png , 
+	creature_elephant.png , 
+	creature_lion.png , 
+	creature_pepocari_bg_white.png , 
+	creature_sabertoothmistbat_bg_white.png , 
+	creature_shrythe.png , 
+	creature_spinosaurus.png , 
+	deco_pokeball.png ,  
+	furniture.png , 
+	game_scorchy.png , 
+	heraldry_lozenge.png , 
+	heraldry_shield.png , 
+	product_greatball.png , 
+	product_pokeball.png , 
+	product_ultraball.png , 
+	stained_glass_bat.png , 
+	stained_glass_turtle.png
+
+*/
+
+var pictureDictionary = { 
+	image00: { address: "product_pokeball.png", name: "Pokeball" },
+	image01: { address: "product_greatball.png", name: "GreatBall" },
+	image02: { address: "product_ultraball.png", name: "Ultraball" },
+	
+	image03: { address: "creature_elephant.png", name: "Elephant" },
+	image04: { address: "creature_bee.png", name: "Bee" },
+	image05: { address: "creature_lion.png", name: "Lion" },
+	
+	image06: {address: "creature_pepocari_bg_white.png", name: "Pepocari Invented Creature"},
+	image07: {address: "creature_sabertoothmistbat_bg_white.png", name: "Sabertooth MistBat Invented Creature"},
+	image08: {address: "creature_shrythe.png", name: "Shrythe Invented Creature"},
+	image09: {address: "creature_armorsnake_lineart.png", name: "Armored Blade Snake Invented Creature Lineart"},
+	image10: {address: "creature_armorsnake.png", name: "Armored Blade Snake Invented Creature"},
+	image11: {address: "stained_glass_bat.png", name: "Stained Glass with Bat Symbol"},
+	image12: {address: "stained_glass_turtle.png", name: "Stainted Glass with Turtle Shell Pattern"},
+	image13: {address: "abstract_professional_hats_bg.png", name: "Abstract Professional Hats"},
+	
+	image14: { address: "deco_pokeball.png", name: "Doodle/Deco style Pokeball" },
+	image15: { address: "comic_gamera.png", name: "Comic Style Gamera (Turtle Kaiju)" },
+	image16: { address: "heraldry_lozenge.png", name: "Heraldry Lozenge" },
+	image17: { address: "heraldry_shield.png", name: "Heraldry Shield" },
+	image18: { address: "furniture.png", name: "Furniture" }
+
+	
+}
+
+var codeBlock = "<div class='container'>" + "<div class = 'row '>";
+var codePiece = "";
+var i = 0;
+
+//console.log(pictureDictionary['image00'].address);
+//<a class="thumbnail" href="#thumb"><img src="media/ocean_thumb.jpg" width="100px" height="66px" border="0" /><span><img src="media/ocean.jpg" /><br />So real, it's unreal. Or is it?</span></a>
+
+
+for (var m in pictureDictionary) {
+	var codePiece =
+		"<div class = ' col-4 col-sm-3 col-lg-2'>"+
+		"<a class='thumbnail' href='#thumb'>"+
+		"<img class=' img-fluid ' src ='../images/" +
+		pictureDictionary[m].address +
+		"' alt='"+
+		pictureDictionary[m].name +
+		"' id = image-" +
+		i +
+		"'>"+
+		
+		"<span >"+
+"<img class=' img-fluid ' src ='../images/" +
+		pictureDictionary[m].address +
+		"' alt='"+
+		pictureDictionary[m].name +
+		"' id = image-" +
+		i + "-" + i +
+		"'>"+
+		"</span>"+
+		
+		"</a><div class= 'text-center'>"+
+		pictureDictionary[m].name +
+		"</div></div>" ;
+	codeBlock += codePiece;
+	i++;
+}
+codeBlock += "</div>" + "</div>";
+
+
+//console.log(codeBlock);
+class ArtGalleryComponent extends HTMLElement {
+	constructor(){
+		super();
+	}
+	
+	connectedCallback() {
+		this.innerHTML = codeBlock;
+		
+		
+	}
+	
+}
+customElements.define('artgallery-component', ArtGalleryComponent);
---
+++ js/navbar.js
@@ -0,0 +1,42 @@
+class NavbarComponent extends HTMLElement {
+	constructor(){
+		super();
+	}
+	
+	connectedCallback() {
+		this.innerHTML = `
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<div class="container-fluid">
+					<a class="navbar-brand" href="index.html">
+						<img alt="Brand" src="../images/favicon-64.png" class="img-fluid">
+					</a>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+							<li class="nav-item">
+								<a class="nav-link active" aria-current="page" href="/index.html">Home</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link active" href="/resume.html">Resume</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link active" href="/artportfolio.html">Art Portfolio</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link disabled" href="#" aria-disabled="true">Implementation Portfolio</a>
+							</li>
+							
+						</ul>
+						
+					</div>
+				</div>
+			</nav>  
+			
+			`;
+	}
+	
+}
+customElements.define('navbar-component', NavbarComponent);
+//<navbar-component></navbar-component>
