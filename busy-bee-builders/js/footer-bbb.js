ace780637dda1570a93386144920e9b12dbce749
Trent Ford <thetrentford@rocketmail.com>
2021-12-29 19:35:01 -0600

    Update Busy Bee Builders Case Study Page

--- busy-bee-builders/js/footer-bbb.js
+++ busy-bee-builders/js/footer-bbb.js
@@ -8,10 +8,11 @@ class FooterComponentBBB extends HTMLElement {
 		<hr>
 			
 		<div class = "text-center">
-		<p><a class="subtle-link" href="about.html" >About Us</a></p>
+		<h6> <a href="../casestudies.html">Back to Case Studies</a></h6>
+
 		
 		
-		<code>  Copyright (c) 2021 Busy Bee Builders </code>
+		<code>  Copyright (c) 2021 Trent N. Ford </code>
 			</div>
 			
 			`;
