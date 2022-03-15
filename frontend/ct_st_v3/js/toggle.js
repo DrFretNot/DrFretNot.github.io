aaccf62bd958ef720fa08ef6b476c30db512a36d
Trent Ford <thetrentford@rocketmail.com>
2022-03-14 19:40:29 -0500

    Add Front End Page and Skill Tests

---
+++ frontend/ct_st_v3/js/toggle.js
@@ -0,0 +1,10 @@
+/* Trent Ford */
+function show1() {
+	document.getElementById("img-m1")
+	.style.visibility = "hidden";
+}
+
+function hide1() {	
+	document.getElementById("img-m1")
+	.style.visibility = "visible";
+}
